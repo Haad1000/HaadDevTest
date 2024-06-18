173868c799c1b3e08917b9a930d4194c0ea24f25
Haad Cheema <haad736@gmail.com>
2024-06-18 11:29:03 -0500

    before editing skills again

--- haad-dev/src/components/Hero.jsx
+++ haad-dev/src/components/Hero.jsx
@@ -9,15 +9,19 @@ const Hero = () => {
   return (
     <section className='flex flex-col lg:flex-row justify-center items-center lg:items-start lg:gap-20 p-16 lg:p-32 text-white mb-8'>
         <div className=' flex flex-col items-center relative m-5 lg:m-0'>
-            <img src={HaadImg2} className='rounded-full w-64 h-80 lg:h-64 object-cover shadow-teal-pfp-shadow hover:shadow-teal-pfp-hover-shadow transition-shadow duration-300 ease-in-out' />
+            <img src={HaadImg2} className='rounded-full w-64 h-80 object-cover shadow-teal-pfp-shadow hover:shadow-teal-pfp-hover-shadow transition-shadow duration-300 ease-in-out' />
         </div>
 
         <div className='flex flex-col items-center text-center m-2 lg:m-5'>
             <h1 className='font-rubik text-5xl'>Hi! I'm <span className='text-teal-400 text-6xl'>Haad Cheema</span></h1>
             <h2 className='font-roboto-condensed text-2xl'>Computer Science Student</h2>
             <span className='flex text-teal-400'>
-                <FaGithub  className='flex-row ml-1 mr-1' size={40}/>
-                <FaLinkedin className='flex-row ml-1 mr-1' size={40}/>
+                <a href='https://github.com/Haad1000'>
+                    <FaGithub  className='flex-row ml-1 mr-1' size={40}/>
+                </a>
+                <a href='https://www.linkedin.com/in/haad-cheema-931105241/'>
+                    <FaLinkedin className='flex-row ml-1 mr-1' size={40}/>
+                </a>
             </span>
             <p className='font-roboto-condensed text-2xl'>
                 A student with a passion for development of all kinds!
