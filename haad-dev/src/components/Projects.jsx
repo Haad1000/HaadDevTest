3c0c157c8bee5f62a929b5ff520519d7e1db458b
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-06-21 23:41:10 -0500

    Finished Project

--- haad-dev/src/components/Projects.jsx
+++ haad-dev/src/components/Projects.jsx
@@ -1,39 +1,49 @@
-import React from 'react';
-import projectsData from './projects.json';
+import React from "react";
+import projectData from "./projectData";
 
 const Projects = () => {
   return (
     <section className="flex flex-col items-center p-12 lg:p-32 text-white mb-8">
       <h1 className="font-rubik text-6xl mb-12 text-teal-300">Projects</h1>
       <div className="w-full max-w-5xl">
-        {projectsData.map((project, index) => (
-          <div 
-            key={index} 
-            className="mb-12 p-8 border border-neutral-600 rounded-lg bg-zinc-800 shadow-lg transition duration-300 transform hover:-translate-y-2"
-          >
-            <h2 className="font-rubik text-3xl mb-2 text-teal-400">{project.title}</h2>
-            <p className="font-roboto-condensed text-lg mb-6 text-neutral-100">{project.description}</p>
-            <div className="flex flex-wrap gap-3 mb-4">
-              {project.technologies.map((tech, index) => (
-                <span 
-                  key={index} 
-                  className="px-4 py-2 bg-teal-700 rounded-full text-sm shadow-sm transition-transform duration-200 transform hover:scale-105 mb-2"
-                >
-                  {tech}
-                </span>
-              ))}
+        {projectData.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4">
+              <img
+                src={project.image}
+                width={200}
+                height={200}
+                alt={project.title}
+                className="mb-6 rounded"
+              />
             </div>
-            <a 
-              href={project.link} 
-              className="px-4 py-2 bg-teal-400 text-black rounded-full hover:bg-teal-600 transition duration-200"
-            >
-              View Project
-            </a>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h2 className="font-rubik text-3xl mb-2 text-teal-400">{project.title}</h2>
+              <p className="font-roboto-condensed text-lg mb-6 text-neutral-100">{project.description}</p>
+              <div className="flex flex-wrap">
+                {project.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-teal-300"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-4 inline-block bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                View Project
+              </a>
+            </div>
           </div>
         ))}
       </div>
     </section>
   );
-}
+};
 
 export default Projects;
---
+++ haad-dev/src/components/projectData.jsx
@@ -0,0 +1,21 @@
+import project01 from "../assets/Haad_02.jpg"
+import GokuLandingPage from "../assets/GokuLandingPage.gif"
+import GojoLandingPage from "../assets/GojoLandingPage.gif"
+
+const projectData = [
+    {
+        "title": "Goku Landing Page",
+        "image": GokuLandingPage,
+        "description": "A beautiful landing page made designed for Goku",
+        "technologies": ["HTML", "CSS", "Bootstrap"],
+        "link": "https://github.com/Haad1000/MUI-Goku-Landing-Page"
+    },
+    {
+        "title": "Gojo Landing Page",
+        "image": GojoLandingPage,
+        "description": "A beautiful landing page made designed for Gojo",
+        "technologies": ["HTML", "CSS", "Bootstrap"],
+        "link": "https://github.com/Haad1000/MUI-Goku-Landing-Page"
+    }
+]
+export default projectData
