17e3fc5769f7c50d794ececb0645fa3845aa9344
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-07-30 07:46:36 -0500

    Added android project

--- haad-dev/src/components/projectData.jsx
+++ haad-dev/src/components/projectData.jsx
@@ -2,26 +2,34 @@ import project01 from "../assets/Haad_02.jpg"
 import GokuLandingPage from "../assets/GokuLandingPage.gif"
 import GojoLandingPage from "../assets/GojoLandingPage.gif"
 import PythonSnakeGame from "../assets/SnakeGame.gif"
+import EnkiAndroidProject from "../assets/ENKI.png"
 
 const projectData = [
     {
+        "title": "Pengu App",
+        "image": EnkiAndroidProject,
+        "description": "A dynamic platform connecting students and teachers with features like live chat, group chats, and a shared calendar for scheduling appointments. Students can search for tutors by subjects, and admins ensure a safe environment by moderating user activities and reports.",
+        "technologies": ["Java"],
+        "link": "https://github.com/Haad1000/Pengu"
+    },
+    {
         "title": "Goku Landing Page",
         "image": GokuLandingPage,
-        "description": "A beautiful landing page designed for Goku",
+        "description": "A beautiful landing page for the anime character Goku, created with HTML, CSS, and Bootstrap.",
         "technologies": ["HTML", "CSS", "Bootstrap"],
         "link": "https://github.com/Haad1000/MUI-Goku-Landing-Page"
     },
     {
         "title": "Gojo Landing Page",
         "image": GojoLandingPage,
-        "description": "A beautiful landing page designed for Gojo",
+        "description": "A beautiful landing page for the anime character Gojo, created with HTML, CSS, and Bootstrap.",
         "technologies": ["HTML", "CSS", "Bootstrap"],
         "link": "https://github.com/Haad1000/Farewell-Gojo-Landing-Page"
     },
     {
         "title": "Snake Game",
         "image": PythonSnakeGame,
-        "description": "A basic snake game made in python",
+        "description": "A classic Snake game built using Python, featuring simple controls and engaging gameplay.",
         "technologies": ["Python"],
         "link": "https://github.com/Haad1000/Snake_Game"
     }
