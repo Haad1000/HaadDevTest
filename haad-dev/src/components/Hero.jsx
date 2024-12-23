44ef471d6d18aa7ad4ee4f3246c1faf065cef168
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-12-23 03:25:14 -0600

    Updated Resume

--- haad-dev/src/components/Hero.jsx
+++ haad-dev/src/components/Hero.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import HaadImg2 from '../assets/Haad_02.jpg';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import oldResume from '../assets/SelfResume.pdf'
+// import oldResume from '../assets/SelfResume.pdf'
+import resume2024 from '../assets/Resume 2024.pdf'
 
 const Hero = () => {
   return (
@@ -32,7 +33,7 @@ const Hero = () => {
           A student with a passion for development of all kinds!
         </p>
         <div className='m-2'>
-          <a href={oldResume} download>
+          <a href={resume2024} download>
             <button className='px-4 py-2 text-lg rounded-full border-teal-300 border-r-2 border-l-2 hover:text-black hover:bg-teal-300 focus:ring-2 focus:ring-cyan-700 transition duration-200 '>
               Resume
             </button>
